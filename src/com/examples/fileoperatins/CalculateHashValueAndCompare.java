97cbac51c12ef66d5e21f81209b04a231473ee4f
shitalkhaire <shitalkhaire.mail@gmail.com>
2018-02-23 19:42:10 +0530

    Added New code in HashValue.java

--- src/com/examples/fileoperatins/CalculateHashValueAndCompare.java
+++ src/com/examples/fileoperatins/CalculateHashValueAndCompare.java
@@ -9,14 +9,7 @@ public class CalculateHashValueAndCompare
 
 	public static void main(String[] args) throws NoSuchAlgorithmException, UnsupportedEncodingException 
 	{
-		
-	// TODO Auto-generated method stub
-
-		byte[] data = "test".getBytes("UTF8");
-        MessageDigest digest = MessageDigest.getInstance("SHA-256");
-        byte[] hash = digest.digest(data);
-        System.out.println(new BASE64Encoder().encode(hash));
-
+	
 
 	}
 
--- src/com/examples/fileoperatins/HashValue.java
+++ src/com/examples/fileoperatins/HashValue.java
@@ -1,139 +1,154 @@
 package com.examples.fileoperatins;
 
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.nio.file.Files;
-import java.nio.file.NoSuchFileException;
-import java.nio.file.Paths;
-import java.security.DigestInputStream;
-import java.security.MessageDigest;
-import java.security.NoSuchAlgorithmException;
+	import java.io.File;
+	import java.io.FileInputStream;
+	import java.io.IOException;
+	import java.io.InputStream;
+	import java.io.InputStreamReader;
+	import java.nio.file.Files;
+	import java.nio.file.NoSuchFileException;
+	import java.nio.file.Paths;
+	import java.security.DigestInputStream;
+	import java.security.MessageDigest;
+	import java.security.NoSuchAlgorithmException;
 
-public class HashValue 
-{
-	public static void main(String[] args) throws NoSuchAlgorithmException, IOException 
+	public class HashValue 
 	{
-		
-	   
-		// Program Using SwitchCase..
-		System.out.println("1.Display All Files");
-		System.out.println("2.Display  HashValue of Files");
-		System.out.println("3.Quit..");
-		
-		int  choice;
-		do
+		public static void main(String[] args) throws NoSuchAlgorithmException, IOException 
 		{
-			System.out.println("Enter your choice:=");
-			BufferedReader b=new BufferedReader(new InputStreamReader(System.in));
-			//System.out.println(+choice);
-			 choice=Integer.parseInt(b.readLine());
-			switch(choice)
-				{
-				
-				case 1:
-					HashValue h1=new HashValue();
-				String strAllFile = null;
-				
-				h1.displayFiles(strAllFile);
+			
+		   
+			// Program Using SwitchCase..
+			System.out.println("1.Display All Files");
+			System.out.println("2.Display  HashValue of Files");
+			System.out.println("3.Quit..");
+			
+			int  choice;
+			do
+			{
+				System.out.println("Enter your choice:=");
+				BufferedReader b=new BufferedReader(new InputStreamReader(System.in));
+				//System.out.println(+choice);
+				 choice=Integer.parseInt(b.readLine());
+				switch(choice)
+					{
 					
-					break;
+					case 1:
+						HashValue h1=new HashValue();
+					String strAllFile = null;
 					
-				case 2:
-					HashValue h=new HashValue();
-					h.getHashofFile("D:\\Test\\file1.txt");      // non-static method
-					//HashValue.getHashofFile("D:\\Test\\file1.txt"); //static method
-					h.getHashofFile("D:\\Test\\file2.txt");
-				
-					//	Files.hash(file, HashValue.getHashofFile()); 
+					h1.displayFiles(strAllFile);
+						
+						break;
+						
+					case 2:
+						//Create checksum for this file
+						File file1 = new File("D:\\Test\\file1.txt");
+						File file2 = new File("D:\\Test\\file2.txt");
+						//Use MD5 algorithm
+						MessageDigest md5Digest = MessageDigest.getInstance("MD5");
+						 
+						//Get the checksum
+						String checksum1 = getFileChecksum(md5Digest, file1);
+						String checksum2 = getFileChecksum(md5Digest, file2);
+				//		boolean isTwoEqual = h1.contentEquals(file1, file2);
+						
+						//see checksum
+						if((checksum1) == (checksum2))
+						//if((checksum1).equals(cheksum2))
+						//Files.asByteSource(file1).contentEquals(Files.asByteSource(file2));
+						{
+						 System.out.println("Given File is not Duplicate..");
+						}
 					
-					break;
-				case 3:
-					System.out.println("Exit from program");
+						System.out.println("Hash of First File: "+checksum1);
+						System.out.println("Hash of Second File: "+checksum2);
 
-				      System.exit(1);
-				      
-				  					
+						break;
+					case 3:
+						System.out.println("Exit from program");
+
+					      System.exit(1);
+					      
+					  					
+					}
+			}while(choice != 3);
+		}
+
+		  
+		String displayFiles(String strAllFile) throws IOException , NoSuchAlgorithmException
+		 {
+			 File folder =new File("D:\\Test\\");
+			 File[] listOfFiles = folder.listFiles();
+				System.out.println("Files location :"+folder);
+				for(int i=0;i<listOfFiles.length;i++)
+				{
+					if(listOfFiles[i].isFile())
+					{
+						System.out.println(""+listOfFiles[i].getName());
+						
+					}
+					else if(listOfFiles[i].isDirectory())
+
+					{
+						System.out.println("Directory location:"+listOfFiles[i]);
+						System.out.println("\nDirectory:\n"+listOfFiles[i].getName());
+						
+
+					}
 				}
-		}while(choice != 3);
+				
+				for (File file : listOfFiles) 
+				{
+				   if (file.isFile()) 
+				   {
+				        System.out.println(file.getName());
+				    }
+				  
+					//return strAllFile;
+				}
+				return strAllFile;
+				
+		 }
+	 static String getFileChecksum(MessageDigest digest, File file) throws IOException
+		{
+		    //Get file input stream for reading the file content
+		    FileInputStream fis = new FileInputStream(file);
+		     
+		    //Create byte array to read data in chunks
+		    byte[] byteArray = new byte[1024];
+		    int bytesCount = 0;
+		      
+		    //Read file data and update in message digest
+		    while ((bytesCount = fis.read(byteArray)) != -1) {
+		        digest.update(byteArray, 0, bytesCount);
+		    };
+		     
+		    //close the stream; We don't need it now.
+		    fis.close();
+		     
+		    //Get the hash's bytes
+		    byte[] bytes = digest.digest();
+		     
+		    //This bytes[] has bytes in decimal format;
+		    //Convert it to hexadecimal format
+		    StringBuilder sb = new StringBuilder();
+		    for(int i=0; i< bytes.length ;i++)
+		    {
+		        sb.append(Integer.toString((bytes[i] & 0xff) + 0x100, 16).substring(1));
+		    }
+		     
+		    //return complete hash
+		   return sb.toString();
+		}
 	}
 
-	 String getHashofFile(String strFileName) throws IOException, NoSuchAlgorithmException
-	{
-		MessageDigest md1 = MessageDigest.getInstance("MD5");
-		
-		//MessageDigest md2 = MessageDigest.getInstance("MD5");
-		
-		System.out.println("file data "+Paths.get("D:\\Test\\file1.txt"));
-		System.out.println("file data "+Paths.get("D:\\Test\\file2.txt"));
-		
-		try (InputStream is = Files.newInputStream(Paths.get(strFileName));
-		 DigestInputStream dis = new DigestInputStream(is, md1)) 
-		{
-		  /* Read decorated stream (dis) to EOF as normal... */
-		}	
-		
-		byte[] digest1 = md1.digest();
-		byte[] digest2 = md1.digest();
-		System.out.println("Hash value of file:-"+digest1);
-	//	return digest1.toString();		
-		System.out.println("Hash value of file:-"+digest2);
-	//Object file;
-	//Files.hash(file, HashValue.md5());    ---added new line here
-		
-//if(true)       when i was used this that time no any problems were shown      
-	return digest1.toString();
-	
-//	return digest2.toString();
+
 		
 			
-		
-		
-	}
-	 
-	
-
-	String displayFiles(String strAllFile) throws IOException , NoSuchAlgorithmException
-	 {
-		 File folder =new File("D:\\Test\\");
-		 File[] listOfFiles = folder.listFiles();
-			System.out.println("Files location :"+folder);
-			for(int i=0;i<listOfFiles.length;i++)
-			{
-				if(listOfFiles[i].isFile())
-				{
-					System.out.println(""+listOfFiles[i].getName());
-					
-				}
-				else if(listOfFiles[i].isDirectory())
-
-				{
-					System.out.println("Directory location:"+listOfFiles[i]);
-					System.out.println("\nDirectory:\n"+listOfFiles[i].getName());
-					
-
-				}
-			}
-			
-			for (File file : listOfFiles) 
-			{
-			   if (file.isFile()) 
-			   {
-			        System.out.println(file.getName());
-			    }
-			  
-				//return strAllFile;
-			}
-			return strAllFile;
-			
-	 }
-}
 
 
-	
-		
 	
 
 		
